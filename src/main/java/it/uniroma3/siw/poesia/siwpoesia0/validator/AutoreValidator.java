96f55ef7796b6307cea3afbc8eb2aa934d1883a8
nenelontano <irene.lontano@gmail.com>
2023-05-31 18:25:20 +0200

    aggiunto validator e logo
    validator per nome autore in modo che non si possano inserire autori con lo stesso nome.
    Logo inserito nell'index ma bisogna ancora fare l'html

---
+++ src/main/java/it/uniroma3/siw/poesia/siwpoesia0/validator/AutoreValidator.java
@@ -0,0 +1,29 @@
+package it.uniroma3.siw.poesia.siwpoesia0.validator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import it.uniroma3.siw.poesia.siwpoesia0.model.Autore;
+import it.uniroma3.siw.poesia.siwpoesia0.repository.AutoreRepository;
+
+@Component
+public class AutoreValidator implements Validator{
+
+	@Autowired
+	private AutoreRepository autoreRepository;
+	
+	@Override
+	public void validate(Object o, Errors errors) {
+		Autore autore = (Autore)o;
+		if(autore.getNome() != null && autoreRepository.existsByNome(autore.getNome())) {
+			errors.reject("autore.duplicate");
+		}
+	}
+	
+	@Override
+	public boolean supports(Class<?> aClass) {
+		return Autore.class.equals(aClass);
+	}
+}
