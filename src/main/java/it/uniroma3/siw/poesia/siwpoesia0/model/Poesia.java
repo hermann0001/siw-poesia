94430e66b92429f75b6ea93262d62693b7a5a436
Hermann <her.tamilia@stud.uniroma3.it>
2023-07-02 15:47:27 +0200

    aggiunto formato della data

--- src/main/java/it/uniroma3/siw/poesia/siwpoesia0/model/Poesia.java
+++ src/main/java/it/uniroma3/siw/poesia/siwpoesia0/model/Poesia.java
@@ -17,6 +17,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.Size;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Table(uniqueConstraints=@UniqueConstraint(columnNames= {"titolo", "autore_id"}))
@@ -40,6 +41,7 @@ public class Poesia {
 	
 	@Column(nullable = false)
 	@PastOrPresent
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private LocalDate data_pubblicazione;
 	
 	//associazioni
