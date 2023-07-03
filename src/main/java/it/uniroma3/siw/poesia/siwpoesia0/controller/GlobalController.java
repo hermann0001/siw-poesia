bc14f2b60e8e4428dc066e529fe51aed4408a70b
Hermann <her.tamilia@stud.uniroma3.it>
2023-07-03 03:26:01 +0200

    cambiata l'autenticazione usando una classe wrapper per la gestione di autore e credentials

--- src/main/java/it/uniroma3/siw/poesia/siwpoesia0/controller/GlobalController.java
+++ src/main/java/it/uniroma3/siw/poesia/siwpoesia0/controller/GlobalController.java
@@ -1,13 +1,11 @@
 package it.uniroma3.siw.poesia.siwpoesia0.controller;
 
+import it.uniroma3.siw.poesia.siwpoesia0.controller.session.SessionData;
+import it.uniroma3.siw.poesia.siwpoesia0.model.Autore;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import it.uniroma3.siw.poesia.siwpoesia0.model.Credenziale;
@@ -19,28 +17,31 @@ import it.uniroma3.siw.poesia.siwpoesia0.service.CredenzialeService;
  * che viene applicato su tutti gli altri controller*/
 @ControllerAdvice
 public class GlobalController {
-	@Autowired 
-	private CredenzialeService credenzialeService;
-	
-	@ModelAttribute("userDetails")
-	public UserDetails getUser() {
-		UserDetails user = null;
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		if (!(authentication instanceof AnonymousAuthenticationToken)) {
-			user = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+	@Autowired
+	private SessionData sessionData;
+
+	@Autowired
+	private CredenzialeService credentialsService;
+
+	@ModelAttribute("user")
+	public Autore getAutore() {
+
+		try{
+			return this.sessionData.getLoggedUser();
 		}
-		return user;
+		catch (ClassCastException e){
+			return null;
+		}
 	}
 
-	@ModelAttribute("credenziali")
-    public Credenziale getCredentials() {
-		Credenziale credenziali = null;
-    	UserDetails user = null;
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (!(authentication instanceof AnonymousAuthenticationToken)) {
-            user = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-            credenziali = this.credenzialeService.getCredentials(user.getUsername());
-        }
-        return credenziali;
+	@ModelAttribute("credentials")
+	public Credenziale getCredentials(){
+		try{
+			UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+			return credentialsService.getCredentials(userDetails.getUsername());
+		}catch(ClassCastException e){
+			return null;
+		}
 	}
 }
---
+++ src/main/java/it/uniroma3/siw/poesia/siwpoesia0/controller/session/SessionData.java
@@ -0,0 +1,61 @@
+package it.uniroma3.siw.poesia.siwpoesia0.controller.session;
+import it.uniroma3.siw.poesia.siwpoesia0.model.Autore;
+import it.uniroma3.siw.poesia.siwpoesia0.model.Credenziale;
+import it.uniroma3.siw.poesia.siwpoesia0.service.AutoreService;
+import it.uniroma3.siw.poesia.siwpoesia0.service.CredenzialeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(value="session", proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class SessionData {
+
+    private Autore autore;
+    private Credenziale credentials;
+    @Autowired
+    private CredenzialeService credentialsService;
+    @Autowired
+    private AutoreService autoreService;
+
+    public Credenziale getLoggedCredentials(){
+        this.update();
+
+        return this.credentials;
+    }
+
+    public Autore getLoggedUser(){
+
+        try{
+            this.update();
+        }catch(ClassCastException e){
+            return null;
+        }
+        return this.autore;
+    }
+
+    private void update(){
+        Object object = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        UserDetails loggedUserDetails = (UserDetails) object;
+
+        this.credentials = this.credentialsService.getCredentials(loggedUserDetails.getUsername());
+        this.autore = this.credentials.getAutore();
+    }
+
+/*    private void oauth2update(){
+        Object object = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        CustomOAuth2User loggedOAuth2User = (CustomOAuth2User) object;
+
+        try {
+            this.user = userRepository.findByName(loggedOAuth2User.getLogin()).get();
+        }
+        catch( NoSuchElementException e ){
+            this.user = userRepository.findByName(loggedOAuth2User.getFullName()).get();
+        }
+    }*/
+
+
+}
