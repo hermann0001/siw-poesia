17fb188772231eaf57e6e84ab592482287c91dd1
Hermann <her.tamilia@stud.uniroma3.it>
2023-07-12 01:32:56 +0200

    aggiunta gestione degli errori personalizzata

--- src/main/java/it/uniroma3/siw/poesia/siwpoesia0/controller/MyErrorController.java
+++ src/main/java/it/uniroma3/siw/poesia/siwpoesia0/controller/MyErrorController.java
@@ -5,25 +5,27 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MyErrorController implements ErrorController {
 
     @RequestMapping("/error")
-    public String handleError(HttpServletRequest request){
+    public String handleError(HttpServletRequest request, Model model){
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
         if (status != null) {
             HttpStatus statusCode = HttpStatus.valueOf(Integer.parseInt(status.toString()));
+            model.addAttribute("status", statusCode);
 
             if(statusCode.is4xxClientError()) {
-                return "error";
+                return "/errors/error404";
             }
             else if(statusCode.is5xxServerError()) {
-                return "error";
+                return "/errors/error500";
             }
         }
-        return "error";
+        return "/errors/error404";
     }
 }
